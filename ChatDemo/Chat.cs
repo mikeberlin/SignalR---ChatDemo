87c921f2f71d02b118b8992926621f8435da0861
Mike Berlin <mikeberlin@me.com>
2014-01-25 14:33:44 -0600

    Initial commit.

---
+++ ChatDemo/Chat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ChatDemo
+{
+    public class Chat : Hub
+    {
+        public void Send(string platform, string message)
+        {
+            Clients.All.messageReceived(platform, message);
+        }
+    }
+}
